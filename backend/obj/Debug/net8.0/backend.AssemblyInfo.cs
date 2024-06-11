aa2deeef7a640fe46b5a65e2394baa18e851b2ba
Gavin Peng <gavpeng7447@gmail.com>
2024-06-10 20:22:05 -1000

    update dockerfile for backend

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9d7e367c399c3ce5b7425a0b0513e3039667f16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e2d8d2b4ee85cd36742be0d77d81b0974dfdf5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
